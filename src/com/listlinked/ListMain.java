4d01b9b4df2576d6cbbeaf3a2cf4596f026fa0cb
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-19 12:16:25 +0530

    Added data at the starting of linked list

---
+++ src/com/listlinked/ListMain.java
@@ -0,0 +1,13 @@
+package com.listlinked;
+
+public class ListMain {
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.insert(70);
+		linkedList.insert(30);
+		linkedList.insert(56);
+		linkedList.display();
+		
+	}
+	
+}
