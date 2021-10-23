e343d78911756a8cf42472b02a2a9128c2bd0cd2
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-23 14:00:33 +0530

    Deleted the node after searching

--- src/com/listlinked/ListMain.java
+++ src/com/listlinked/ListMain.java
@@ -20,5 +20,9 @@ public class ListMain {
 		Node newNode = new Node(40);
 		linkedList.searchNode(30, newNode);
 		linkedList.display();
+		
+		System.out.println("The deleted element is: 40");
+		linkedList.deleteSearchNode(40);
+		linkedList.display();
 		}
 }
